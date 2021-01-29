ab18937522e267b21a9fcad7686a2c3cbfed7099
aragrevo <eduver_san@hotmail.com>
2021-01-29 07:03:42 -0500

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'banner',
+    label: 'Inicio',
+  },
+  {
+    path: 'services',
+    label: 'Servicios',
+  },
+  {
+    path: 'testimonials',
+    label: 'Aliados',
+  },
+  {
+    path: 'news',
+    label: 'Ventajas',
+  },
+];
